c040e2bb442a55b46b96eeb68a847fb559cf805b
Stefano Varesi <stefano.varesi@gmail.com>
2018-10-17 17:29:42 +0200

    added support for main http verbs in routing builder

--- src/main/kotlin/io/github/xstefanox/RoutingBuilder.kt
+++ src/main/kotlin/io/github/xstefanox/RoutingBuilder.kt
@@ -3,7 +3,11 @@ package io.github.xstefanox
 import io.undertow.server.HttpServerExchange
 import io.undertow.server.RoutingHandler
 import io.undertow.util.HttpString
+import io.undertow.util.Methods.DELETE
 import io.undertow.util.Methods.GET
+import io.undertow.util.Methods.PATCH
+import io.undertow.util.Methods.POST
+import io.undertow.util.Methods.PUT
 
 typealias Handler = (HttpServerExchange) -> Unit
 
@@ -13,6 +17,22 @@ class RoutingBuilder {
 
     fun get(template: String, httpHandler: Handler) {
         templates[template] = mapOf(GET to httpHandler)
+    }
+
+    fun post(template: String, httpHandler: Handler) {
+        templates[template] = mapOf(POST to httpHandler)
+    }
+
+    fun put(template: String, httpHandler: Handler) {
+        templates[template] = mapOf(PUT to httpHandler)
+    }
+
+    fun patch(template: String, httpHandler: Handler) {
+        templates[template] = mapOf(PATCH to httpHandler)
+    }
+
+    fun delete(template: String, httpHandler: Handler) {
+        templates[template] = mapOf(DELETE to httpHandler)
     }
 
     fun build(): RoutingHandler {
--- src/test/kotlin/io/github/xstefanox/RoutingBuilderTest.kt
+++ src/test/kotlin/io/github/xstefanox/RoutingBuilderTest.kt
@@ -4,11 +4,28 @@ import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.StringSpec
 import io.restassured.RestAssured
 import io.undertow.Undertow
+import io.undertow.server.RoutingHandler
 import org.apache.http.HttpStatus.SC_OK
 
 class RoutingBuilderTest : StringSpec({
 
     System.setProperty("org.jboss.logging.provider", "slf4j")
+
+    fun assertWithUndertow(port : Int, routingHandler: RoutingHandler, block: () -> Unit) {
+
+        val undertow = Undertow.builder()
+                .addHttpListener(port, "0.0.0.0")
+                .setHandler(routingHandler)
+                .build()
+
+        undertow.start()
+
+        try {
+            block()
+        } finally {
+            undertow.stop()
+        }
+    }
 
     "routing builder should produce a new object on every execution" {
 
@@ -25,21 +42,73 @@ class RoutingBuilderTest : StringSpec({
         val routingBuilder = RoutingBuilder()
         routingBuilder.get("/test") {}
 
-        val undertow = Undertow.builder()
-                .addHttpListener(8282, "0.0.0.0")
-                .setHandler(routingBuilder.build())
-                .build()
+        assertWithUndertow(8282, routingBuilder.build()) {
 
-        undertow.start()
-
-        try {
             RestAssured.given()
                     .get("http://localhost:8282/test")
                     .then()
                     .assertThat()
                     .statusCode(SC_OK)
-        } finally {
-            undertow.stop()
+        }
+    }
+
+    "POST request should be added to the handler" {
+
+        val routingBuilder = RoutingBuilder()
+        routingBuilder.post("/test") {}
+
+        assertWithUndertow(8282, routingBuilder.build()) {
+
+            RestAssured.given()
+                    .post("http://localhost:8282/test")
+                    .then()
+                    .assertThat()
+                    .statusCode(SC_OK)
+        }
+    }
+
+    "PUT request should be added to the handler" {
+
+        val routingBuilder = RoutingBuilder()
+        routingBuilder.put("/test") {}
+
+        assertWithUndertow(8282, routingBuilder.build()) {
+
+            RestAssured.given()
+                    .put("http://localhost:8282/test")
+                    .then()
+                    .assertThat()
+                    .statusCode(SC_OK)
+        }
+    }
+
+    "PATCH request should be added to the handler" {
+
+        val routingBuilder = RoutingBuilder()
+        routingBuilder.patch("/test") {}
+
+        assertWithUndertow(8282, routingBuilder.build()) {
+
+            RestAssured.given()
+                    .patch("http://localhost:8282/test")
+                    .then()
+                    .assertThat()
+                    .statusCode(SC_OK)
+        }
+    }
+
+    "DELETE request should be added to the handler" {
+
+        val routingBuilder = RoutingBuilder()
+        routingBuilder.delete("/test") {}
+
+        assertWithUndertow(8282, routingBuilder.build()) {
+
+            RestAssured.given()
+                    .delete("http://localhost:8282/test")
+                    .then()
+                    .assertThat()
+                    .statusCode(SC_OK)
         }
     }
 })
